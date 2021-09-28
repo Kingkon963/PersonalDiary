91d0e1a06b5ebfdb67b301c05f23a0420114d802
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-09-28 19:14:37 +0600

    Dashboard init

---
+++ src/main/java/com/example/personaldiary/controllers/DashboardController.java
@@ -0,0 +1,11 @@
+package com.example.personaldiary.controllers;
+
+import model.global.User;
+
+public class Dashboard {
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/model/global/User.java
@@ -0,0 +1,2 @@
+package model.global;public class User {
+}
